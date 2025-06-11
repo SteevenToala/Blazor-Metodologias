d99a0c5ecdc0c7ad13b6f6d5494315b8491abd4b
Alexander Tasinchano <132286484+Alexande6055@users.noreply.github.com>
2025-06-11 01:15:01 -0500

    implemetnacion de peticion al backend para el login

--- MyCleanApp.Client/Program.cs
+++ MyCleanApp.Client/Program.cs
@@ -8,5 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:3000/") });
+builder.Services.AddScoped<AuthService>();
 
 await builder.Build().RunAsync();
---
+++ MyCleanApp.Client/Services/Auth.cs
@@ -0,0 +1,55 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+public class LoginRequest
+{
+    public string correo { get; set; } = string.Empty;
+    public string contraseña { get; set; } = string.Empty;
+}
+
+public class LoginResponse
+{
+    public string Token { get; set; } = string.Empty;
+    public Usuario? usuario { get; set; } = null;
+
+}
+public class Usuario
+{
+    public string Nombre { get; set; } = string.Empty;
+    public string Correo { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
+public class AuthService
+{
+    private readonly HttpClient _http;
+
+    public AuthService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<LoginResponse?> LoginAsync(LoginRequest loginData)
+    {
+        try
+        {
+            var response = await _http.PostAsJsonAsync("http://localhost:5015/api/Auth/login", loginData);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<LoginResponse>();
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error en login: {error}");
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al hacer POST: {ex.Message}");
+            return null;
+        }
+    }
+}
