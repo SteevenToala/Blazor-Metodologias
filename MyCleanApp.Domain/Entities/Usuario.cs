fdb00cc09344d2551a7be9d187a2af712af14175
Alexander Tasinchano <132286484+Alexande6055@users.noreply.github.com>
2025-06-10 23:52:38 -0500

    implementacion de entidad usuario

---
+++ MyCleanApp.Domain/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+namespace MyCleanApp.Domain.Entities
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string contraseña { get; set; }
+        public string rol { get; set; }
+
+    }
+}
