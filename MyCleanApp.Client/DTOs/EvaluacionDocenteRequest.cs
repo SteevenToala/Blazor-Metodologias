c7498968dedcb06961254803e20bc903e91c452f
Jaml888 <jaimadiel888@gmail.com>
2025-07-03 23:38:35 -0500

    fix: cambiar FechaEvaluacion a nullable en EvaluacionDocenteRequest

--- MyCleanApp.Client/DTOs/EvaluacionDocenteRequest.cs
+++ MyCleanApp.Client/DTOs/EvaluacionDocenteRequest.cs
@@ -5,7 +5,7 @@ namespace MyCleanApp.Client.DTOs
         public string Periodo { get; set; } = string.Empty;
         public double Puntaje { get; set; }
         public int DocenteId { get; set; }
-        public DateTime FechaEvaluacion { get; set; }
+        public DateTime? FechaEvaluacion { get; set; }
         public string TipoEvaluacion { get; set; } = string.Empty;
         public string? Observaciones { get; set; }
         public byte[]? Certificado { get; set; }
