0cb7476850c559ed926ba19a73b06abbe467e244
Steeven Toala <steevenpfb@gmail.com>
2025-07-04 01:53:44 -0500

    se mejoro la interfaz para subir capacitaciones evaluaciones y publicaciones

--- MyCleanApp.Client/DTOs/CursoCapacitacionCreateReques.cs
+++ MyCleanApp.Client/DTOs/CursoCapacitacionCreateReques.cs
@@ -1,8 +1,8 @@
 public class CursoCapacitacionCreateReques
 {
     public string Nombre { get; set; } = string.Empty;
-    public DateTime FechaInicio { get; set; }
-    public DateTime FechaFin { get; set; }
+    public DateTime? FechaInicio { get; set; }
+    public DateTime? FechaFin { get; set; }
     public string Certificado { get; set; } = string.Empty;
     public int Horas { get; set; }
     public bool Externo { get; set; } = false;
